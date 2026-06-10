41b0a41bafa0559385d9396978b3366acbc41195
BestLeaks <59940456+BestLeaks@users.noreply.github.com>
2026-06-09 22:47:42 -0400

    Update resource description and changelog
    rebrand to dynamicpd

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Mega Utilities\' Mega Group'
-description 'This resource checks for updates for the callout fivepd-json'
+description 'This resource checks for updates for the callout & plugin dynamicpd'
 version '1.0.0'
 
 
