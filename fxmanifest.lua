c8a9bd888b670db498c742b73ba9058a0f1d6e7b
BestLeaks <59940456+BestLeaks@users.noreply.github.com>
2026-06-10 00:09:13 -0400

    Bump version to 1.0.1 and update changelog
    Updated version to 1.0.1 and added changelog entries.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,13 +3,14 @@ game 'gta5'
 
 author 'Mega Utilities\' Mega Group'
 description 'This resource checks for updates for the callout & plugin dynamicpd'
-version '1.0.0'
+version '1.0.1'
 
 
 server_script 'updater.lua'
 
 changelog [[
-- Added changelog support in updater
+- Rebrand to dynamicpd
+- Possible fix for cleanup bug (https://github.com/mega-group/dynamicpd-fivpd/issues/1#issuecomment-3272407943)
 ]]
 
 
